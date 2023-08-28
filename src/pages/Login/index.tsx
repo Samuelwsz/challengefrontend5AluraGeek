9f93fe144d0d0c169ccba70cf028fd2dd60f30c2
Samuelwsz <Samuelxwsz@gmail.com>
2023-08-27 21:47:35 -0300

    att

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,6 +2,9 @@ import styled from "@emotion/styled"
 import Botao from "../../components/Botao"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 const AreaInputs = styled.form`
   display: flex;
@@ -20,6 +23,16 @@ const InputEstilizado = styled.input`
   outline: none;
 `
 
+const schema = z.object({
+  email: z
+    .string()
+    .nonempty("O e-mail é obrigatorio")
+    .email("Formato de e-mail inválido"),
+  password: z.string().min(4, "A senha precisa ter pelo menos 4 caracteres"),
+})
+
+type FormProps = z.infer<typeof schema>
+
 export default function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
@@ -27,37 +40,45 @@ export default function Login() {
 
   const navigate = useNavigate()
 
-  const handleLogin = () => {
-    if (email === "u" && password === "1") {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<FormProps>({ resolver: zodResolver(schema) })
+
+  const handleLogin = (data: any) => {
+    if (email === "usuario@gmail.com" && password === "1234") {
       setError(false)
-      /* setLoggedIn(true)*/
       navigate("/addproduto")
-      console.log("logou")
     } else {
       setError(true)
     }
+    console.log(data)
   }
-
 
   return (
     <>
       <div style={{ background: "#f5f5f5" }}>
-        <AreaInputs>
+        <AreaInputs onSubmit={handleSubmit(handleLogin)}>
           <h2 style={{ marginBottom: "30px" }}>Iniciar Sessão</h2>
           <InputEstilizado
+            {...register("email")}
             type="email"
             placeholder="Escreva seu email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+          {errors.email && <span>{errors.email.message}</span>}
           <InputEstilizado
+            {...register("password")}
             type="password"
             placeholder="Escreva sua senha"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+          {errors.password && <span>{errors.password.message}</span>}
           {error && <p style={{ color: "red" }}>Credenciais incorretas</p>}
-          <Botao variante="secundaria" titulo="Entrar" onClick={handleLogin} />
+          <Botao variante="secundaria" titulo="Entrar" tipo="submit" />
         </AreaInputs>
       </div>
     </>
